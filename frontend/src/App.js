dcefdfc832288dc4e81c4329a6e0f3aa8b20f29f
illu1996 <illu140411@gmail.com>
2023-07-24 09:18:18 +0900

    component and Route

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import RouteLink from './routes/Route';
+import './App.css';
+import { Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Link to="/login">로그인</Link>
+      <Link to="/signup">회원가입</Link>
+
+      <RouteLink></RouteLink>
+    </div>
+  );
+}
+
+export default App;
