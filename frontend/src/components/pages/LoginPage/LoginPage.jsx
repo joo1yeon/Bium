dcb56381a50aa54381a6dc47d8144a3e0d49b840
최지수 <cjjss11@naver.com>
2023-07-24 16:51:35 +0900

    login 초기

--- frontend/src/components/pages/LoginPage/LoginPage.jsx
+++ frontend/src/components/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import login from '../../../slices/userSlice.js';
 
-export const login = () => {
-  return <div>login</div>;
-};
+function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const dispatch = useDispatch()
 
-export default login;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+
+    const handleSubmit = (e)=> {
+
+        if (!emailRegex.test(email)) {
+            alert('유효한 이메일 형식이 아닙니다.');
+            return;
+        }
+        if (!passwordRegex.test(password)) {
+            alert("비밀번호는 최소 8자 이상이어야 하며, 영문 대소문자와 숫자를 포함해야 합니다.");
+            return;
+        }
+        dispatch(login({ email,  password }));
+    }
+    return (
+        <div>
+            <form onSubmit={(e)=> handleSubmit(e)}>
+                <div className='loginId'>
+                    <div>
+                        <input 
+                        type="input" id="userEmail" placeholder='이메일을 입력해 주세요.' 
+                        value={email} onChange={(e)=> setEmail(e.target.value)}/>
+                    </div>
+                </div>
+                <div className='loginPassword'>
+                    <div>
+                        <input 
+                        type="password" id="userPassword" placeholder='비밀번호를 입력해 주세요.' 
+                        value={password} onChange={(e)=> setPassword(e.target.value)}/>
+                    </div>
+                </div>
+                <div>
+                    <button className='loginButton'>로그인</button>
+                </div>
+            </form>
+        </div>
+        
+    )
+}
+
+
+export default Login
