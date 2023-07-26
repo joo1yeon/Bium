73a8165332350f01c97c6cd12012b29fd998e2d1
qhfhdls12 <qhfhdls12@naver.com>
2023-07-26 11:56:39 +0900

    remove: backend 폴더 삭제

--- backend/src/main/java/com/ssafy/bium/gameroom/repository/GameRoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.bium.gameroom.repository;
-
-import com.ssafy.bium.gameroom.GameRoom;
-import org.springframework.data.repository.CrudRepository;
-
-public interface GameRoomRepository extends CrudRepository<GameRoom, Long> {
-}
