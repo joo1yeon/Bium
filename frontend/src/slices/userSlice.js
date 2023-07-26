c2fd8986cbdf4d757fd6189a94e684b00e179aad
최지수 <cjjss11@naver.com>
2023-07-26 16:26:48 +0900

    axios

--- frontend/src/slices/userSlice.js
+++ frontend/src/slices/userSlice.js
@@ -2,16 +2,22 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialStateValue= {email: "", password: ""}
 
-export const userSlice = createSlice({
+const userSlice = createSlice({
     name: "user",
     initialState: { value: initialStateValue},
     reducers: {
-        login: (state, action) => {
-            state.value = action.payload
-        },
+        // login: (state, action) => {
+        //     state.value = action.payload
+        // },
+        getUserInfo(state,action) {
+            state.value = action.payload;
+        }
     },
 });
 
-export const { login } = userSlice.actions;
+//api 폴더에 axios 요청 코드를 작성하고
+// 여기에 acions를 만들어서 actions 안에 
+// api 폴더에서 만든 axios를 불러온다
+export const loginActions = userSlice.actions;
 
 export default userSlice.reducer;
