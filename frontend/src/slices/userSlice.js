bd23dae6578973449e1d16fa3113a82148278171
최지수 <cjjss11@naver.com>
2023-07-26 16:29:14 +0900

    axios

--- frontend/src/slices/userSlice.js
+++ frontend/src/slices/userSlice.js
@@ -6,18 +6,12 @@ const userSlice = createSlice({
     name: "user",
     initialState: { value: initialStateValue},
     reducers: {
-        // login: (state, action) => {
-        //     state.value = action.payload
-        // },
         getUserInfo(state,action) {
             state.value = action.payload;
         }
     },
 });
 
-//api 폴더에 axios 요청 코드를 작성하고
-// 여기에 acions를 만들어서 actions 안에 
-// api 폴더에서 만든 axios를 불러온다
 export const loginActions = userSlice.actions;
 
 export default userSlice.reducer;
