c25caa75867f72dcf03a545d3e3ee4cdd567ad46
김동현 <qhfhdls12@naver.com>
2023-07-26 23:42:20 +0900

    feat: 로그인 간단 구현

--- backend/src/main/java/com/ssafy/bium/user/repository/UserRepository.java
+++ backend/src/main/java/com/ssafy/bium/user/repository/UserRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserEmailAndUserPw(String userEmail, String userPw);
 
-    Optional<User> findByUserEmailAndUserPw(String UserEmail, String UserPw);
-    Optional<User> findByLoginId(String userEmail);
-    Optional<User> findById(Long id);
 }
