6832ad29a5f8e44ca71af0a3d1b7ced91bf3d8c3
손효민 <sondo100@hanmail.net>
2023-07-28 03:33:00 +0900

    feat: 회원가입

--- backend/src/main/java/com/ssafy/bium/user/request/UserRegisterPostReq.java
+++ backend/src/main/java/com/ssafy/bium/user/request/UserRegisterPostReq.java
@@ -1,10 +1,24 @@
 package com.ssafy.bium.user.request;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 public class UserRegisterPostReq {
+    String userEmail;
+    String userPw;
+    String userName;
+    String userNickname;
 
+    @Builder
+    public UserRegisterPostReq(String userEmail, String userPw, String userName, String userNickname){
+        this.userEmail = userEmail;
+        this.userPw = userPw;
+        this.userName = userName;
+        this.userNickname = userNickname;
+    }
 }
