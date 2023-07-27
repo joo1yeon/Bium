1135bb17274731bea49aeb48e808a14bdd24f94e
손효민 <sondo100@hanmail.net>
2023-07-28 04:39:02 +0900

    refactor : 불필요한 코드 삭제

--- backend/src/main/java/com/ssafy/bium/user/repository/UserRepository.java
+++ backend/src/main/java/com/ssafy/bium/user/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.ssafy.bium.user.repository;
 
 import com.ssafy.bium.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
