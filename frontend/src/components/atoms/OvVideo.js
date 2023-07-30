5c63ee64a18fd2f89186f54248041e2b1fc7222f
illu1996 <illu140411@gmail.com>
2023-07-31 01:30:50 +0900

    feat:face-api 추가

--- frontend/src/components/atoms/OvVideo.js
+++ frontend/src/components/atoms/OvVideo.js
@@ -4,6 +4,35 @@ import * as faceapi from 'face-api.js';
 const OpenViduVideoComponent = (props) => {
   console.log('제발 빨리 끝내고 잘 수 있으면 좋겠다', props);
   const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  //모델 불러오기
+  const loadModels = () => {
+    console.log('start');
+    const MODEL_URL = process.env.PUBLIC_URL + '/models';
+
+    Promise.allSettled([
+      // THIS FOR FACE DETECT AND LOAD FROM YOU PUBLIC/MODELS DIRECTORY
+      faceapi.nets.ssdMobilenetv1.loadFromUri(MODEL_URL),
+      faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+      faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+      faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+      faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL)
+
+      // faceapi.nets.face
+    ]).then((e) => {
+      console.log(e);
+      console.log('end');
+
+      faceMyDetect();
+    });
+  };
+
+  // 변경마다;
+  useEffect(() => {
+    videoRef && loadModels();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   useEffect(() => {
     if (props && videoRef.current) {
@@ -11,7 +40,42 @@ const OpenViduVideoComponent = (props) => {
     }
   }, [props]);
 
-  return <video autoPlay={true} ref={videoRef} />;
+  //내 이미지로부터 인식하고 다시 그려주기
+  const faceMyDetect = () => {
+    setInterval(async () => {
+      console.log(videoRef.current);
+      const videoElement = document.querySelector('#localVideo');
+      const detections = await faceapi
+        .detectSingleFace(videoElement, new faceapi.TinyFaceDetectorOptions())
+        .withFaceLandmarks()
+        .withFaceExpressions();
+
+      console.log(detections);
+
+      // DRAW YOU FACE IN WEBCAM
+      canvasRef.current.innerHTML = faceapi.createCanvasFromMedia(videoElement);
+      faceapi.matchDimensions(canvasRef.current, {
+        width: 480,
+        height: 270
+      });
+
+      const resized = faceapi.resizeResults(detections, {
+        width: 480,
+        height: 270
+      });
+
+      faceapi.draw.drawDetections(canvasRef.current, resized);
+      faceapi.draw.drawFaceLandmarks(canvasRef.current, resized);
+      faceapi.draw.drawFaceExpressions(canvasRef.current, resized);
+    }, 8000);
+  };
+
+  return (
+    <>
+      <video id="localVideo" autoPlay={true} ref={videoRef} />
+      <canvas id="drawCanvas" ref={canvasRef} />
+    </>
+  );
 };
 
 export default OpenViduVideoComponent;
