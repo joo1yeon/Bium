13da987f7de7db97c1ffd54f897b0a674d6f5cf9
최지수 <cjjss11@naver.com>
2023-07-31 17:57:08 +0900

    feat: 로그인 - JWT 토큰

--- frontend/src/slices/getLoginInfo.js
+++ frontend/src/slices/getLoginInfo.js
@@ -1,16 +1,24 @@
 import { setIsLogin, setIsLoginError, setIsValidToken, setUserInfo } from './userSlice';
 import jwtDecode from 'jwt-decode';
-import { login, findById, tokenRegeneration } from './login';
+import { findById, tokenRegeneration } from './login';
+import axios from 'axios';
 
 // 사용자 로그인 처리 함수
 export const userLogin = (user) => async (dispatch) => {
   try {
-    const response = await login(user);
-    if (response.message === 'success') {
-      console.log(response.data);
-      const { accessToken: accessToken, refreshToken: refreshToken } = response;
+    const response = await axios
+      .post('http://localhost:8080/login', user)
+      .then((response) => {
+        return response;
+      })
+      .catch((err) => {
+        return err;
+      });
+    console.log(response);
+    if (response.data.message === 'success') {
+      const accessToken = response.data.httpHeaders;
       sessionStorage.setItem('accessToken', accessToken);
-      sessionStorage.setItem('refreshToken', refreshToken);
+      // sessionStorage.setItem('refreshToken', refreshToken);
       dispatch(setIsLogin(true));
       dispatch(setIsLoginError(false));
       dispatch(setIsValidToken(true));
@@ -28,13 +36,15 @@ export const userLogin = (user) => async (dispatch) => {
 export const getUserInfo = (token) => async (dispatch) => {
   try {
     const decodedToken = jwtDecode(token);
-    const response = await findById(decodedToken.sub);
-    if (response.message === 'success') {
-      // 토큰이 유효하다면 setUserInfo 액션을 디스패치하여 사용자 정보를 가져와 상태를 업데이트
-      dispatch(setUserInfo(response.userInfo));
-    } else {
-      console.log('유저 정보 없음');
-    }
+    console.log(decodedToken);
+    const response = await axios
+      .get('http://localhost:8080/login', { headers: { Authorization: decodedToken.sub } })
+      .then((response) => {
+        return response;
+      })
+      .catch((err) => {
+        return err;
+      });
   } catch (error) {
     console.error('토큰 만료되어 사용 불가능', error.response.status);
     dispatch(setIsValidToken(false));
