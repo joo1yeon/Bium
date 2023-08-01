9b1daebd7993615bad06d0f9f43bdbb1f55ff417
illu1996 <illu140411@gmail.com>
2023-08-01 12:13:34 +0900

    feat: 게임생성 및 게임방 페이지 분리

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import RouteLink from './routes/Route';
 import './App.css';
-import { Link } from 'react-router-dom';
+import RouteLink from './routes/Route';
+import { NavBar } from './components/atoms/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <Link to="/login">로그인</Link>
-      <Link to="/signup">회원가입</Link>
-      <Link to="/room">게임방입장</Link>
-
+      <NavBar></NavBar>
       <RouteLink></RouteLink>
     </div>
   );
