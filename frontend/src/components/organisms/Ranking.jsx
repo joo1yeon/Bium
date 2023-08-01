4985e3598124e7ae1ccc11919a58126f754d904a
이정찬 <wjdcks1203@naver.com>
2023-08-01 16:10:14 +0900

    feat: 랭킹 조회 작성

--- frontend/src/components/organisms/Ranking.jsx
+++ frontend/src/components/organisms/Ranking.jsx
@@ -1,42 +1,45 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useSelector } from 'react';
 import axios from 'axios';
-import { useDispatch } from 'react-redux';
-import { getUserInfo } from '../../../slices/getLoginInfo';
+// import { setProfile } from '../../../slices/userSlice';
 import { Fragment } from 'react';
-import RankingItem from '../molecules/RankingItem'; 
+import RankingItem from '../molecules/RankingItem';
 
 function useRanking() {
-
+  // 헤더 인증용 토큰
   const [rank, setRank] = useState([]);
+  const nickname = useSelector((state) => state.user.nickname);
+  const todayBium = useSelector((state) => state.user.todayBium);
+  const totalBium = useSelector((state) => state.user.totalBium);
+  // 사용자 등수는 어떻게???
 
   useEffect(() => {
     // 랭킹 요청
-    axios.get('http://localhost:8080/profile/ranking')
-      .then ((response) => {
+    axios
+      .get('http://localhost:8080/profile/ranking')
+      .then((response) => {
         setRank(response.data);
-      }) 
-      .catch ((error) => {
+      })
+      .catch((error) => {
         console.error(error);
       });
   }, []);
-  
+
   return (
     <Fragment>
-      <div className='Ranking title'>
+      <div className="Ranking title">
         <p>Ranking</p>
       </div>
       <div>
-        <div className='Ranking list'>
-          순위 
+        <div></div>
+        <div className="Ranking list">
+          {rank.map((item, index) => (
+            <RankingItem key={index} index={index} item={item} />
+          ))}
         </div>
-        {rank.map((item, index) => (
-          <RankingItem key={index} index={index} item={item} />
-        ))}
+        <p>...</p>
       </div>
     </Fragment>
   );
-  
-};
+}
 
-
-export {useRanking} ;
+export { useRanking };
