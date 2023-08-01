3dd1d759a28fe55b2b9c883ec62b2ed33ed7afa5
illu1996 <illu140411@gmail.com>
2023-08-01 20:56:26 +0900

    fix:파일명 수정

--- frontend/src/slices/videoSlice/videoThunkActionSlice.js
+++ frontend/src/slices/videoSlice/videoThunkActionSlice.js
@@ -1,5 +1,4 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-
 import axios from 'axios';
 
 const APPLICATION_SERVER_URL = process.env.NODE_ENV === 'production' ? '' : 'https://demos.openvidu.io/';
