1d02d609d2ee88433cc1ba5bf8146cb40b2188d5
최지수 <cjjss11@naver.com>
2023-08-01 15:59:30 +0900

    feat: 게임 목록 조회 폼

--- frontend/src/routes/Route.jsx
+++ frontend/src/routes/Route.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import MainPage from '../components/pages/MainPage/MainPage';
 import LoginPage from '../components/pages/LoginPage/LoginPage';
 import SignupPage from '../components/pages/SignupPage/SignupPage';
-import GameMakePage from '../components/pages/GameMakePage/GameMakePage';
+import GameReadyPage from '../components/pages/GameReadyPage/GameReadyPage';
 
 function RouteLink() {
   return (
@@ -11,6 +11,7 @@ function RouteLink() {
         <Route path="/" element={<MainPage />} />
         <Route path="/login/" element={<LoginPage />} />
         <Route path="/signup/" element={<SignupPage />} />
+        <Route path="/gameready/" element={<GameReadyPage />} />
       </Routes>
     </div>
   );
