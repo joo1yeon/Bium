f6d6d37962ed7b336535d0abb2d4758edf05e74e
illu1996 <illu140411@gmail.com>
2023-08-01 17:47:31 +0900

    fix: 불필요한 컴포넌트 제거'

--- frontend/src/components/atoms/NavBar.jsx
+++ frontend/src/components/atoms/NavBar.jsx
@@ -8,7 +8,9 @@ export const NavBar = () => {
       <Link to="/api/signup">회원가입/ </Link>
       <Link to="/api/createroom">게임방 생성/ </Link>
       <Link to="/api/room">게임방입장/ </Link>
-      <Link to="/api/profile/:nickname">프로필 화면</Link>
+      <Link to="/api/profile/:nickname">프로필 화면/ </Link>
+      <Link to="/api/gameready"> 게임방 생성/ </Link>
+      <Link to="/api/gamemake"> 메이크/ </Link>
     </div>
   );
 };
--- frontend/src/components/pages/ProfilePage/ProfilePage.jsx
+++ frontend/src/components/pages/ProfilePage/ProfilePage.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Fragment } from 'react';
 
 export const ProfilePage = () => {
   return (
