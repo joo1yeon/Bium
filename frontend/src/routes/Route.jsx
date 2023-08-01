cab7b252ea7f17923d7ac9414d4bfa8ef7afd076
illu1996 <illu140411@gmail.com>
2023-08-01 21:10:12 +0900

    fix : delete ameroom duplication

--- frontend/src/routes/Route.jsx
+++ frontend/src/routes/Route.jsx
@@ -2,10 +2,9 @@ import { Route, Routes } from 'react-router-dom';
 import MainPage from '../components/pages/MainPage/MainPage';
 import LoginPage from '../components/pages/LoginPage/LoginPage';
 import SignupPage from '../components/pages/SignupPage/SignupPage';
-import GameReadyPage from '../components/pages/GameReadyPage/GameReadyPage';
+import GameRoomListPage from '../components/pages/GameRoomListPage/GameRoomListPage';
 import GameRoomPage from '../components/pages/GameRoomPage/GameRoomPage';
-import GameMakePage from '../components/pages/GameMakePage/GameMakePage';
-import { CreateGameRoom } from '../components/pages/CreateGameRoomPage/CreateGameRoomPage';
+import CreateGameRoom from '../components/pages/CreateGameRoomPage/CreateGameRoomPage';
 import { ProfilePage } from '../components/pages/ProfilePage/ProfilePage';
 
 function RouteLink() {
@@ -16,10 +15,9 @@ function RouteLink() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/createroom" element={<CreateGameRoom />} />
-        <Route path="/room" element={<GameRoomPage />} />
+        <Route path="/gameroomlist" element={<GameRoomListPage />} />
+        <Route path="/gameroom" element={<GameRoomPage />} />
         <Route path="/profile/:nickname" element={<ProfilePage />} />
-        <Route path="/gameready" element={<GameReadyPage />} />
-        <Route path="/gamemake" element={<GameMakePage />} />
       </Routes>
     </div>
   );
