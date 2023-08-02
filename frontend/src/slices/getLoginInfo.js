95f787e7ce4e1a2d3b5e4d8061bda30dcf3ac60d
이정찬 <wjdcks1203@naver.com>
2023-08-02 16:09:56 +0900

    feat: 프로필 화면

--- frontend/src/slices/getLoginInfo.js
+++ frontend/src/slices/getLoginInfo.js
@@ -5,7 +5,7 @@ import {
   setUserEmail,
   setNickname,
   setTodayBium,
-  setTotalBium,
+  setTotalBium
 } from './userSlice';
 
 import axios from 'axios';
@@ -42,19 +42,18 @@ export const userLogin = (user) => async (dispatch) => {
 
 // 사용자 정보를 가져오는 동작 처리 함수
 export const getUserInfo = (Email) => async (dispatch) => {
-
   try {
     const response = await axios.get(`http://localhost:8080/info/${Email}`);
     // console.log('함수 확인');
     console.log(response.data.userInfo);
-    dispatch(setUserEmail(response.data.userInfo.userEmail ));
-    dispatch(setNickname( response.data.userInfo.userNickname ));
-    dispatch(setTodayBium( response.data.userInfo.todayBium ));
-    dispatch(setTotalBium( response.data.userInfo.totalBium ));
-    console.log('setUserEmail입니다.', (setUserEmail(response.data.userInfo.userEmail )));
-    console.log('setNickname입니다.', (setNickname(response.data.userInfo.userNickname )));
-    console.log('setTodayBium입니다.', (setTodayBium(response.data.userInfo.todayBium )));
-    console.log('setTotalBium입니다.', (setTotalBium(response.data.userInfo.totalBium )));
+    dispatch(setUserEmail(response.data.userInfo.userEmail));
+    dispatch(setNickname(response.data.userInfo.userNickname));
+    dispatch(setTodayBium(response.data.userInfo.todayBium));
+    dispatch(setTotalBium(response.data.userInfo.totalBium));
+    console.log('setUserEmail입니다.', setUserEmail(response.data.userInfo.userEmail));
+    console.log('setNickname입니다.', setNickname(response.data.userInfo.userNickname));
+    console.log('setTodayBium입니다.', setTodayBium(response.data.userInfo.todayBium));
+    console.log('setTotalBium입니다.', setTotalBium(response.data.userInfo.totalBium));
     return response.data;
   } catch (error) {
     console.error('토큰 만료되어 사용 불가능', error.response.status);
