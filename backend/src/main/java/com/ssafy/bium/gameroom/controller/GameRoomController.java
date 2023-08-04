a1bc058f53d7c2c379723f845b3c3d87846d0166
illu1996 <illu140411@gmail.com>
2023-08-04 13:52:20 +0900

    CORS ..

--- backend/src/main/java/com/ssafy/bium/gameroom/controller/GameRoomController.java
+++ backend/src/main/java/com/ssafy/bium/gameroom/controller/GameRoomController.java
@@ -16,7 +16,7 @@ import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/game")
+@RequestMapping("/game")
 @CrossOrigin(origins = "*")
 public class GameRoomController {
     private final GameRoomService gameRoomService;
@@ -31,11 +31,11 @@ public class GameRoomController {
 
     @PostMapping("/create")
     public String create(
-            @RequestBody GameRoomDto request,
-            @RequestParam String userEmail
+            @RequestBody GameRoomDto request
+//            @RequestParam String userEmail
     ) throws OpenViduJavaClientException, OpenViduHttpException {
         // 방 생성
-        return gameRoomService.createGameRoom(request, userEmail);
+        return gameRoomService.createGameRoom(request, "io@gmail.com");
     }
 
     @PostMapping("/enter")
