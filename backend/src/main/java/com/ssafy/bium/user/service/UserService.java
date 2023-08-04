7bbfb5c5fc8ac70115973eb38bac0ddfb58d36b4
연주원 <joo1yeon@naver.com>
2023-08-04 15:53:01 +0900

    chore: 임시 저장

--- backend/src/main/java/com/ssafy/bium/user/service/UserService.java
+++ backend/src/main/java/com/ssafy/bium/user/service/UserService.java
@@ -3,7 +3,9 @@ package com.ssafy.bium.user.service;
 import com.ssafy.bium.user.User;
 
 import com.ssafy.bium.user.request.UserLoginPostReq;
+import com.ssafy.bium.user.request.UserModifyPostReq;
 import com.ssafy.bium.user.request.UserRegisterPostReq;
+import com.ssafy.bium.user.response.UserModifyGetRes;
 
 
 public interface UserService {
@@ -12,4 +14,7 @@ public interface UserService {
     User getUserByUserEmail(String userEmail);
     int deleteUserByUserEmail(String userEmail);
     public User login(UserLoginPostReq userLoginPostReq);
+    UserModifyGetRes getModifyData(String userEmail);
+    int modifyProfile(UserModifyPostReq userModifyPostReq);
+
 }
