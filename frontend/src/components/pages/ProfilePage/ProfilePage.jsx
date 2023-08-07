bf27f5f86a587ddcbff665783e5a99294557f690
이정찬 <wjdcks1203@naver.com>
2023-08-07 11:54:06 +0900

    fix: persist 초기화 해결

--- frontend/src/components/pages/ProfilePage/ProfilePage.jsx
+++ frontend/src/components/pages/ProfilePage/ProfilePage.jsx
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react';
 import styles from './ProfilePage.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, useNavigate } from 'react-router-dom';
-import { setIsLogin, setUserEmail } from '../../../slices/userSlice';
+import { setIsLogin, setNickname, setUserEmail } from '../../../slices/userSlice';
 import { GetRanking } from '../../organisms/RankingList';
 import useGetBiumTime from '../../../hooks/TimeInquery';
 import axios from 'axios';
+import { persistor } from '../../../store/store';
 
 export function ProfilePage() {
   const { userEmail } = useParams();
@@ -21,7 +22,7 @@ export function ProfilePage() {
   // console.log(confirmIslogin);
 
   // 회원 정보 수정의 기본값은 store 기본값에 한정
-  const [nickname, setNickname] = useState(savedNickname);
+  const [name, setName] = useState(savedNickname);
   const [password, setPassword] = useState('');
   const [passwordConfirm, setPasswordConfirm] = useState('');
   const todayBium = useGetBiumTime(savedTodayBium);
@@ -43,18 +44,34 @@ export function ProfilePage() {
 
   const modifyUserInfo = async (e) => {
     e.preventDefault();
-    if (password !== passwordConfirm) {
-      return true;
-    }
+    try {
+      if (password !== passwordConfirm) {
+        return alert('비밀번호가 일치하지 않습니다.');
+      }
 
-    const response = axios.get(` http://localhost:8080/api/profile/modify/${savedEmail}`, {
-      nickname,
-      password
-    });
-
-    if (response.status === 200) {
-      // 통신이 성공한 경우 변경된 닉네임을 다시 스토어에 넣어준다.
-      dispatch(setNickname(response.data.userInfo.userNickname));
+      const data = {
+        userEmail: savedEmail,
+        userNickname: name,
+        userPw: password
+      };
+      const response = await axios.post(`http://localhost:8080/api/profile/modify`, data, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Methods': 'POST'
+          // Authorization: `Bearer ${accessToken}`
+        }
+      });
+      
+      console.log(response.data);
+      if (response.status === 200) {
+        console.log('회원 정보 수정 성공');
+        dispatch(setNickname(name));
+        // setName(updatedNickname);
+        persistor.flush();
+      }
+    } catch (error) {
+      console.error('회원 정보 수정에 실패하였습니다.', error);
     }
   };
 
@@ -92,14 +109,8 @@ export function ProfilePage() {
   };
 
   useEffect(() => {
-    // 세션 스토리지에서 userEmail 값을
-    const sessionUserEmail = window.sessionStorage.getItem('userEmail');
-
-    // 세션 스토리지와 스토어의 userEmail 값이 다른 경우 스토어를 업데이트합니다.
-    if (sessionUserEmail && (!userEmail || userEmail === '')) {
-      dispatch(setUserEmail(sessionUserEmail));
-    }
-  }, [dispatch]);
+    
+  }, []);
 
   return (
     <>
@@ -116,10 +127,16 @@ export function ProfilePage() {
           <div className={styles.modal}>
             <h2>회원정보 수정</h2>
             <form>
-              <div>{savedNickname}</div>
+              <div>{name}</div>
               <label>
                 닉네임:
-                <input type="text" value={nickname} onChange={(e) => setNickname(e.target.value)} />
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                />
               </label>
               <br />
               <label>
