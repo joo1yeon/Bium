45aaea251f36e3d32034fb819188eb5a6c7c6ce4
illu1996 <illu140411@gmail.com>
2023-08-07 18:07:12 +0900

    feat: 타이머 시작하기

--- frontend/src/components/atoms/Timer/Timer.jsx
+++ frontend/src/components/atoms/Timer/Timer.jsx
@@ -1,31 +1,85 @@
 import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import styles from './Timer.module.css';
 
-export default function Timer() {
+export default function GameTimer() {
   // 시간을 담을 변수
-  const [count, setCount] = useState(0);
+  const [totalCount, setTotalCount] = useState(0);
+  const [ovSecond, setOvSecond] = useState(0);
+  const [ovMinute, setOvMinute] = useState(0);
+  const [ovHour, setOvHour] = useState(0);
+
+  const [start, setStart] = useState(false);
+
+  const publisher = useSelector((state) => state.video.publisher);
 
   useEffect(() => {
+    console.log(start);
+    console.log('왜 타이머 시작 안해?');
     // 설정된 시간 간격마다 setInterval 콜백이 실행된다.
-    const startTimer = setInterval(() => {
-      // 타이머 숫자가 하나씩 줄어들도록
-      setCount((count) => count + 1);
-    }, 1000);
-
-    // 0이 되면 카운트가 멈춤
-    if (count === 100) {
-      clearInterval(startTimer);
+    if (start === true) {
+      console.log('여기 실행햬?');
+      const startTimer = setInterval(() => {
+        setTotalCount((totalCount) => totalCount + 1);
+      }, 1000);
+      const secToHour = Math.floor(totalCount / 3600);
+      const secToMinute = Math.floor((totalCount - secToHour * 3600) / 60);
+      const secToSec = totalCount % 60;
+      setOvHour(secToHour);
+      setOvMinute(secToMinute);
+      setOvSecond(secToSec);
+      return () => {
+        // clearInterval(startTimer);
+        // setStart(false);
+        console.log('여기는 count를 초기화 하는 곳');
+        // setTotalCount(0);
+      };
     }
-    return () => clearInterval(startTimer);
     // 카운트 변수가 바뀔때마다 useEffecct 실행
-  }, [count]);
+  }, [totalCount]);
+
+  const startSignal = (publisher) => {
+    console.log('여기는 스트림 매니저', publisher);
+    const data = {
+      message: 'start'
+    };
+    publisher.stream.session.signal({
+      data: JSON.stringify(data),
+      type: 'timer'
+    });
+    console.log('타이머 시작', data);
+
+  };
+
+  useEffect(() => {
+    if (start && publisher !== undefined) {
+      console.log('쿠키 세션에 이벤트 추가', publisher);
+      publisher.stream.session.on('signal:timer', (e) => {
+        console.log(e);
+        const data = JSON.parse(e.data);
+        console.log(data);
+        if (data.message === 'start') {
+          setStart(true);
+        }
+      });
+    }
+  }, [start]);
 
   return (
-    <div className={styles.timer}>
-      <p>
-        <b>당신의 비움 시간</b>
-      </p>
-      <span>{count}</span>
+    <div>
+      <div className={styles}>
+        <button
+          onClick={() => {
+            startSignal(publisher);
+          }}
+        >
+          Start
+        </button>
+        <p>당신의 비움 시간</p>
+        <p>{ovHour}</p>
+        <p>{ovMinute}</p>
+        <p>{ovSecond}</p>
+      </div>
     </div>
   );
 }
