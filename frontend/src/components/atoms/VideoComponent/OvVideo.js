ff7db32ab89e510a0d7dd10f8585c88a38429ccf
illu1996 <illu140411@gmail.com>
2023-08-09 13:12:21 +0900

    fix: conflict 해결

--- frontend/src/components/atoms/VideoComponent/OvVideo.js
+++ frontend/src/components/atoms/VideoComponent/OvVideo.js
@@ -10,7 +10,6 @@ const OpenViduVideoComponent = (props) => {
 
   console.log('제발 빨리 끝내고 잘 수 있으면 좋겠다', gameFallCount);
   const videoRef = useRef(null);
-  const canvasRef = useRef(null);
 
   //모델 불러오기
   const loadModels = () => {
@@ -32,7 +31,6 @@ const OpenViduVideoComponent = (props) => {
     if (join) {
       videoRef && loadModels();
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
@@ -43,48 +41,25 @@ const OpenViduVideoComponent = (props) => {
   }, [props]);
 
   useEffect(() => {
-    const faceMyDetect = (props) => {
-      console.log(props);
-      const gameFallCount = props.gameFallCount;
-      setInterval(async () => {
-        const videoElement = document.querySelector('#localVideo');
-        // DRAW YOU FACE IN WEBCAM
-        if (videoRef.current !== null) {
-          const detections = await faceapi.detectSingleFace(videoElement, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
-          if (detections && detections.expressions.neutral < 0.6) {
-            dispatch(setGameFallCount(gameFallCount + 1));
-            console.log(detections.expressions);
-            console.log(gameFallCount);
-          }
-          // canvasRef.current.innerHTML = faceapi.createCanvasFromMedia(videoElement);
-          // faceapi.matchDimensions(canvasRef.current, {
-          //   width: 480,
-          //   height: 270
-          // });
-          // if (detections) {
-          //   const resized = faceapi.resizeResults(detections, {
-          //     width: 480,
-          //     height: 270
-          //   });
+    const faceMyDetect = async () => {
+      const videoElement = document.querySelector('#localVideo');
+      console.log('얼굴시작');
 
-          //   faceapi.draw.drawDetections(canvasRef.current, resized);
-          //   faceapi.draw.drawFaceLandmarks(canvasRef.current, resized);
-          //   faceapi.draw.drawFaceExpressions(canvasRef.current, resized);
-          // }
+      // DRAW YOU FACE IN WEBCAM
+      if (videoRef.current !== null) {
+        const detections = await faceapi.detectSingleFace(videoElement, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+        if (detections && detections.expressions.neutral < 0.6) {
+          dispatch(setGameFallCount(gameFallCount + 1));
+          console.log(detections.expressions);
+          console.log(gameFallCount);
         }
-      }, 2000);
-    };
-    faceMyDetect({ gameFallCount });
-    return () => {
-      console.log('clear');
-      clearInterval(faceMyDetect);
+      }
     };
   }, []);
 
   return (
     <>
       <video id="localVideo" audio="false" autoPlay={true} ref={videoRef} />
-      <canvas id="drawCanvas" ref={canvasRef} />
     </>
   );
 };
