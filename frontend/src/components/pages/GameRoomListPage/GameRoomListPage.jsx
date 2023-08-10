accceff758b6b91ea81cd2c362645d94d616144c
joo1yeon <jooowon65@naver.com>
2023-08-10 15:33:27 +0900

    design: 게임방 목록

--- frontend/src/components/pages/GameRoomListPage/GameRoomListPage.jsx
+++ frontend/src/components/pages/GameRoomListPage/GameRoomListPage.jsx
@@ -26,8 +26,24 @@ export const GameRoomListPage = () => {
 
   return (
     <div>
-      <h1>게임방 목록</h1>
-      <div className={styles.container}>
+      <div className={styles.containerTitle}>
+        <div className={styles.title}>
+          <h1>게임방 목록</h1>
+          <a className={styles.BtnGameCreate}>
+            <svg width="20" height="20" viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg">
+              <path fill="currentColor" d="M228 128a12 12 0 0 1-12 12h-76v76a12 12 0 0 1-24 0v-76H40a12 12 0 0 1 0-24h76V40a12 12 0 0 1 24 0v76h76a12 12 0 0 1 12 12Z"/>
+            </svg>
+          </a>
+        </div>
+        <div className={styles.search}>
+          <input type="text" className={styles.search__input} placeholder="게임방 검색"></input>
+          <button className={styles.search__button}>
+            🔍  
+          </button>
+        </div>
+      </div>
+      <div className={styles.containerItems}>
+
         {allRooms !== [] ? (
           <>
             {allRooms.map((allRoom, index) => {
