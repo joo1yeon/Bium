11574f2174b8c8395a72d9020510a4a8cfd1780f
최지수 <cjjss11@naver.com>
2023-08-11 10:07:37 +0900

    fix: confilct 해결

--- frontend/src/components/pages/LoginPage/LoginPage.jsx
+++ frontend/src/components/pages/LoginPage/LoginPage.jsx
@@ -56,22 +56,6 @@ function LoginPage() {
   const handlePasswordChange = (e) => {
     setUserPw(e.target.value);
   };
-  
-  const goToSignUp = () => {
-    return navigate('/signup');
-  };
-
-  const goToMainPage = () => {
-    return navigate('/');
-  };
-
-  const goToSignUp = () => {
-    return navigate('/signup');
-  };
-
-  const goToMainPage = () => {
-    return navigate('/');
-  };
 
   const goToSignUp = () => {
     return navigate('/signup');
@@ -109,7 +93,6 @@ function LoginPage() {
         <div className={styles.leftcontent}>
           <p>아직 비움의 회원이 아니세요?</p>
           <h3 onClick={goToSignUp}>회원가입</h3>
-<<<<<<< HEAD
         </div>
       </div>
 
@@ -151,56 +134,11 @@ function LoginPage() {
           </button>
         </div>
         <div>
-=======
-        </div>
-      </div>
-
-      <div className={styles.rightbox}>
-        <h1 className={styles.titlelogin}>로그인</h1>
-        <form onSubmit={handleSubmit}>
-          <div className={styles.loginid}>
-            <label htmlFor="userEmail">
-              이메일 <br></br>
-              <input
-                type="text"
-                id="userEmail"
-                placeholder="이메일을 입력해 주세요."
-                value={userEmail}
-                onChange={handleEmailChange}
-              />
-            </label>
-
-          </div>
-          <div className={styles.loginpassword}>
-            <label htmlFor="userPassword">
-              비밀번호 <br></br>
-              <input
-                type="password"
-                id="userPassword"
-                placeholder="비밀번호를 입력해 주세요."
-                value={userPw}
-                onChange={handlePasswordChange}
-              />
-            </label>
-          </div>
-          <div className={styles.buttoncontainer}>
-            <button className={styles.cancelbutton} onClick={goToMainPage}>
-              취소
-            </button>
-            <button className={styles.loginbutton}>로그인</button>
-          </div>
-        </form>
-        <div>
-          <button onClick={openModal} className={styles.findpasswordbutton}>
-            비밀번호 찾기
-          </button>
->>>>>>> 85af08ad7fd182873366c7aec91075d2b8fe7ac4
           {passwordModal && (
             <div className={styles.modal}>
               <h2>비밀번호 찾기</h2>
               <form>
                 <label>
-<<<<<<< HEAD
                   이메일
                   <input
                     type="text"
@@ -208,10 +146,6 @@ function LoginPage() {
                     className={styles.passwordinput}
                     onChange={(e) => setPasswordEmail(e.target.value)}
                   ></input>
-=======
-                  이메일:
-                  <input type="text" value={passwordEmail} onChange={(e) => setPasswordEmail(e.target.value)}></input>
->>>>>>> 85af08ad7fd182873366c7aec91075d2b8fe7ac4
                 </label>
               </form>
               <br></br>
