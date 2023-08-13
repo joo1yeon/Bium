81554ac39ceab618f6c8eee9e8bb377f05127ad9
이지혁 <illu140411@gmail.com>
2023-08-14 07:49:48 +0900

    style: gameroom css 작업

--- frontend/src/components/atoms/VideoComponent/UserVideoComponent.js
+++ frontend/src/components/atoms/VideoComponent/UserVideoComponent.js
@@ -2,11 +2,10 @@ import React, { useState } from 'react';
 import OpenViduVideoComponent from './OvVideo';
 import { useSelector } from 'react-redux';
 import emptyprofile from '../../../../src/asset/backgroudimage/emptyprofile.png';
+import styles from './UserVideoComponent.module.css';
+import { IoRocket } from 'react-icons/io5';
 
 const UserVideoComponent = (props) => {
-  const getNicknameTag = () => {
-    return JSON.parse(props.streamManager.stream.connection.data).clientData;
-  };
   const OV = useSelector((state) => state.video.OV);
   const session = useSelector((state) => state.video.session);
   const subscribers = useSelector((state) => state.video.subscribers);
@@ -15,30 +14,32 @@ const UserVideoComponent = (props) => {
   const [disturb, setDisturb] = useState(false);
   const start = useSelector((state) => state.room.start);
   return (
-    <div>
-      <div>
-        <p>{getNicknameTag()}</p>
-      </div>
+    <div className={styles.videoBox}>
       {props.streamManager !== undefined ? (
-        <div className="streamcomponent">
-          {props.streamManager === publisher ? (
-            <button
-              onClick={() => {
-                setDisturb(true);
-              }}
-            >
-              방해하기 버튼
-            </button>
-          ) : null}
-
+        <>
           {disturb && start === false ? (
             <>
               <img src={emptyprofile} alt="사진이 없어..." />
             </>
           ) : (
-            <OpenViduVideoComponent streamManager={props.streamManager} />
+            <div className={styles.userVideos}>
+              <OpenViduVideoComponent streamManager={props.streamManager} />
+              {props.streamManager === publisher ? (
+                <button
+                  className={styles.disturbButton}
+                  onClick={() => {
+                    setDisturb(true);
+                  }}
+                >
+                  <i className={styles.iTag}>
+                    <IoRocket className={styles.rocketIcon} />
+                  </i>
+                  <span className={styles.spanTag}>Disturb</span>
+                </button>
+              ) : null}
+            </div>
           )}
-        </div>
+        </>
       ) : null}
     </div>
   );
