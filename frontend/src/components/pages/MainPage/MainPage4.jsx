d9bebe7a2a3055066899b119f3119292b612beec
이지혁 <illu140411@gmail.com>
2023-08-16 15:21:44 +0900

    design:메인페이지 디자인

--- frontend/src/components/pages/MainPage/MainPage4.jsx
+++ frontend/src/components/pages/MainPage/MainPage4.jsx
@@ -1,16 +1,27 @@
 import styles from './MainPage4.module.css';
 import image3 from '../../../asset/mainImage/full.png';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function MainPage4() {
+  const navigate = useNavigate();
+  const isLogin = useSelector((state) => state.user.isLogin);
+  const token = useSelector((state) => state.user.token);
+
+  const goToGameRooom = () => {
+    return token === null || isLogin === false ? navigate('/login') : navigate('/gameroomlist');
+  };
   return (
     <div className={styles.main4}>
-      <p>이용 안내</p>
+      <p className={styles.main4title}>이용 안내</p>
       <div className={styles.main4box}>
         <div className={styles.mian4order}>
           <p className={styles.order1}>1. 비움방 입장</p>
           <p className={styles.order2}>2. 생각 비우기</p>
           <p className={styles.order3}>3. 비움 즐기기</p>
-          <p className={styles.order4}>-> 즐기러 가기</p>
+          <p className={styles.order4} onClick={goToGameRooom}>
+            즐기러 가기
+          </p>
         </div>
         <img className={styles.image3} src={image3} alt="" />
       </div>
