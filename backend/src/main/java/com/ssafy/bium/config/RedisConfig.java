dd109ced09c2c8d10e733dad8661143c06ee7fb2
qhfhdls12 <qhfhdls12@naver.com>
2023-08-18 04:12:49 +0900

    refactor: 코드 정리

--- backend/src/main/java/com/ssafy/bium/config/RedisConfig.java
+++ backend/src/main/java/com/ssafy/bium/config/RedisConfig.java
@@ -1,11 +1,8 @@
 package com.ssafy.bium.config;
 
-import com.ssafy.bium.gameroom.request.GameRoomDto;
-import com.ssafy.bium.gameroom.request.SearchGameRoomDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
